2ce37e8935dc42f0e4cc95af9facdfdd8b72d942
Fernanda Udala <fernanda.amorim@claravista.com.br>
2021-05-25 01:11:35 -0300

    fix

--- push.js
+++ push.js
@@ -7,6 +7,14 @@ let vapidKeys = {
 
 push.setVapidDetails('fernanda:fernanda.amorim@claravista.com.br', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+    endpoint:
+    "https://fcm.googleapis.com/fcm/send/cTPTsJ9ZWIM:APA91bFNYkihSu4DjVMTx1-VsUDcPiwyUhI3YNOK7pDwEq_YquQrt7G7zZppdnuEfy06kdmnWlVCExN9R5u1XP4xcCe2yDTISXWdwMnTff7BQPj3dtyuIzxam6ibJ1N9iAG6HQYcY20s",
+    expirationTime:null,
+    keys:{
+        p256dh:"BH93dPRGNlFObru9LVg3oA3Ss8z3sZV_GiayFZN1TTon2gTAi0FXJQNS09UkQ8-fSwBLjf-LdYnv27r3SdSebfI",
+        auth:"4xQYo10MqJcC_mHtl9V69Q"
+    }
+}; 
 
 push.sendNotification(sub, 'test message')
